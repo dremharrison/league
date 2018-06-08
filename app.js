89ec45a03dd10106ae031c453c14a99b573bc226
deandremharrison <deandremharrison@gmail.com>
2018-06-08 18:42:27 -0400

    add navbar and fix pics

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('views/images'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
